b5b16d79b73b96990b240a328838446e94a70937
Traian <traian.plosca@gmail.com>
2022-12-21 16:10:59 +0200

    connect & test local db

--- manager/postgresql_manager.py
+++ manager/postgresql_manager.py
@@ -1,9 +1,18 @@
 import psycopg2
+import os
 
 
 class PostgresqlManager:
 
     def __init__(self):
-        conn = psycopg2.connect(database="trinance_test",
-                        user='postgres', password='pass',
-                        host='127.0.0.1', port='5432')
+        self.conn = psycopg2.connect(database=os.getenv("POSTGRES_DB_TEST"),
+                                user=os.getenv("POSTGRES_DB_USER"), password=os.getenv("POSTGRES_DB_PWD"),
+                                host='127.0.0.1', port='5432')
+        self.conn.autocommit = True
+        self.cursor = self.conn.cursor()
+
+    def test(self):
+        sql3 = '''select * from personal_investment;'''
+        self.cursor.execute(sql3)
+        for i in self.cursor.fetchall():
+            print(i)
--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-from util.csv_converter import CSVConverter
+from util import CSVConverter
+from manager import PostgresqlManager
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
+    load_dotenv()   # load env file
     csv_data = CSVConverter("Investment.csv")
     print(csv_data.get_csv_data())
+    pm = PostgresqlManager()
+    pm.test()
 
