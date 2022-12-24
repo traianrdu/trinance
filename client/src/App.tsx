c9f7c9c071ff0afe34fd306fea3b402a3679fa4f
Traian <traian.plosca@gmail.com>
2022-12-24 14:08:17 +0200

    create sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Sidebar from "./components/Sidebar";
-import MainRoutes from "./MainRoutes";
+import Sidebar from './components/Sidebar';
+import MainRoutes from './MainRoutes';
+import './App.css'
 
 function App() {
     return (
-        <div className="App">
+        <div className="container">
             <Sidebar/>
             <MainRoutes/>
         </div>
--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React from 'react';
+import {IconContext} from 'react-icons';
+import {NavLink} from 'react-router-dom';
+import './Sidebar.css';
+
+const classNameFunc = ({ isActive } : {isActive:any}) => (isActive ? "Sidebar-button" : "Sidebar-button"); // either is active or not, it will show the same css
 
 export default function Sidebar() {
     return (
-        <div className='sidebar'>
-            Side bar
-        </div>
+        <nav className="Sidebar">
+            <NavLink className={classNameFunc} to='/'>
+                Dashboard
+            </NavLink>
+            <NavLink className={classNameFunc} to='/dashboard'>
+                Dashboard
+            </NavLink>
+        </nav>
     );
 }
