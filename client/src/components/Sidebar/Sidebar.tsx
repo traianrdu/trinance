3c3e904db0820e8a9bfbfd32279504cc5c08e33c
Traian <traian.plosca@gmail.com>
2022-12-24 16:12:17 +0200

    add sidebar design

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import {RxDashboard} from 'react-icons/rx';
 import {IconContext} from 'react-icons';
 import {NavLink} from 'react-router-dom';
 import './Sidebar.css';
+import {AiOutlineHome} from 'react-icons/ai';
 
-const classNameFunc = ({ isActive } : {isActive:any}) => (isActive ? "Sidebar-button" : "Sidebar-button"); // either is active or not, it will show the same css
+const classNameFunc = ({ isActive } : { isActive:any }) => (isActive ? "Sidebar-button" : "Sidebar-button"); // either is active or not, it will show the same css
 
 export default function Sidebar() {
     return (
-        <nav className="Sidebar">
-            <NavLink className={classNameFunc} to='/'>
-                Dashboard
-            </NavLink>
-            <NavLink className={classNameFunc} to='/dashboard'>
-                Dashboard
-            </NavLink>
-        </nav>
+        <IconContext.Provider value={{ className: "shared-class", size: "32" }}>
+            <nav className="Sidebar">
+                <NavLink className={classNameFunc} to='/'>
+                    <AiOutlineHome />
+                    <span className={"Sidebar-span"}>trinance</span>
+                </NavLink>
+                <NavLink className={classNameFunc} to='/dashboard'>
+                    <RxDashboard />
+                    <span className={"Sidebar-span"}>Dashboard</span>
+                </NavLink>
+            </nav>
+        </IconContext.Provider>
     );
 }
