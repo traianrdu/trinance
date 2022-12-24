99780cacbb148e8ffd07f1b3940f2c498d5cff87
Traian <traian.plosca@gmail.com>
2022-12-24 02:19:42 +0200

    fix routes

--- client/src/MainRoutes.tsx
+++ client/src/MainRoutes.tsx
@@ -1,16 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Dashboard from "./containers/Dashboard";
 
 export default function MainRoutes() {
     return (
-        <Router>
-            <div className="container">
-                <Routes>
-                    <Route path='/' element={<Dashboard />} />
-                    <Route path='/dashboard' element={<Dashboard />} />
-                </Routes>
-            </div>
-        </Router>
+        <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
     );
 }
