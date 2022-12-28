11aed1749675cc157c4f39298d6f063fd59f149e
Traian <traian.plosca@gmail.com>
2022-12-29 01:23:33 +0200

    create api requests + calls

--- server.py
+++ server.py
@@ -1,7 +1,17 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__, static_url_path='')
+CORS(app)
 app.config["DEBUG"] = True
+
+
+@app.route("/test1", methods=['GET', 'POST'])
+def test1():
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+    return {"status": 0, "statusText": "good", "data": "good data", "error": "none"}
 
 
 @app.route("/", methods=['GET', 'POST'])
