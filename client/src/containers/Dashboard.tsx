0cddf565b9770bf3678fd4758f573e057747d2eb
Traian <traian.plosca@gmail.com>
2023-04-20 00:42:20 +0300

    feat: parse response from server

--- client/src/containers/Dashboard.tsx
+++ client/src/containers/Dashboard.tsx
@@ -17,6 +17,15 @@ export default function Dashboard() {
         if (response.loading === Status[Status.loaded]) {
             response.loading = Status[Status.init];
             console.log(response.status, response.statusText, response.data, response.error, response.loading);
+            if (response.status === 200) {
+                //console.log(response.data);
+                //console.log(JSON.parse(decodeURI(response.data)));
+                //const ceva: string = response.data.toString().replace(/\\"/g, '"');
+                //const ceva2 = JSON.parse(ceva);
+                let responseData = JSON.stringify(response.data);
+                let parsedJsonResponseData = JSON.parse(responseData);
+                console.log(parsedJsonResponseData.data.currency);
+            }
         }
     }, [response]);
 
