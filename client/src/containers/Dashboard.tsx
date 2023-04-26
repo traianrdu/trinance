9f7fa4f926a7a187483b6fab392ad28082455c24
Traian <traian.plosca@gmail.com>
2023-04-26 15:49:01 +0300

    feat: add dashboard graph

--- client/src/containers/Dashboard.tsx
+++ client/src/containers/Dashboard.tsx
@@ -24,15 +24,16 @@ ChartJS.register(
 );
 
 export default function Dashboard() {
-    const [isLoadedDashboard, setIsLoadedDashboard] = useState(false);
-    const [isAPICall, setIsAPICall] = useState(false);
-    const [selectedCurrency, setSelectedCurrency] = useState("");
-    const [isEmptyPrice, setIsEmptyPrice] = useState(false);
-    const [dataList, setDataList] = useState<any[]>([]);
-    const [dateList, setDateList] = useState<any[]>([]);
-    const [incomeList, setIncomeList] = useState<any[]>([]);
-    const [expensesList, setExpensesList] = useState<any[]>([]);
+    const [isLoadedDashboard, setIsLoadedDashboard] = useState(false);  // dashboard first loading
+    const [isAPICall, setIsAPICall] = useState(false);  // api call
+    const [selectedCurrency, setSelectedCurrency] = useState("");   // selected currency
+    const [isEmptyPrice, setIsEmptyPrice] = useState(false);    // empty price check
+    const [dataList, setDataList] = useState<any[]>([]);    // full list of data
+    const [dateList, setDateList] = useState<any[]>([]);    // list of timestamps
+    const [incomeList, setIncomeList] = useState<any[]>([]);    // income list
+    const [expensesList, setExpensesList] = useState<any[]>([]);    // expenses list
 
+    // graph options
     const options = {
         responsive: true,
         interaction: {
@@ -40,68 +41,94 @@ export default function Dashboard() {
             intersect: false,
         },
         stacked: false,
-  plugins: {
-    title: {
-      display: true,
-      text: 'Chart.js Line Chart - Multi Axis',
-    },
-  },
-  scales: {
-    y: {
-      type: 'linear' as const,
-      display: true,
-      position: 'left' as const,
-    },
-    y1: {
-      type: 'linear' as const,
-      display: true,
-      position: 'right' as const,
-      grid: {
-        drawOnChartArea: false,
-      },
-    },
-  },
-};
+        plugins: {
+            title: {
+                display: true,
+                text: 'Income-Expenses',
+            },
+        },
+        scales: {
+            y: {
+                type: 'linear' as const,
+                display: true,
+                position: 'left' as const,
+            },
+            y1: {
+                type: 'linear' as const,
+                display: true,
+                position: 'right' as const,
+                grid: {
+                    drawOnChartArea: false,
+                },
+            },
+        },
+    };
+
+    // set graph data
+    const [graphData, setGraphData] = useState({
+        dateList,
+        datasets: [
+            {
+                label: "income",
+                data: incomeList,
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+                label: "expenses",
+                data: expensesList,
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+        ],
+    });
 
     const response: Response  = useApiGet('http://192.168.0.66:5000/dashboard/get-dashboard-data');
 
     useEffect(() => {
         if (!isLoadedDashboard) {
-            setIsLoadedDashboard(true);
+            setIsLoadedDashboard(true);  // dashboard was loaded
             response.loading = Status[Status.init];  // reset response
-            response.useAPI();
-            setIsAPICall(true);
+            response.useAPI();  // api call
+            setIsAPICall(true); // set api call check
         }
         // api response
         if (response.loading === Status[Status.loaded] && isAPICall) {
-            setIsAPICall(false);
-            response.loading = Status[Status.init];
+            setIsAPICall(false);    // reset api call check
+            response.loading = Status[Status.init]; // reset status
             console.log(response.status, response.statusText, response.data, response.error, response.loading);
             if (response.status === 200) {
-                //console.log(response.data);
-                //console.log(JSON.parse(decodeURI(response.data)));
-                //const ceva: string = response.data.toString().replace(/\\"/g, '"');
-                //const ceva2 = JSON.parse(ceva);
-                let stringResponse = JSON.stringify(response.data);
-                let parsedJsonResponse = JSON.parse(stringResponse);
-                if (parsedJsonResponse.status === true) {
-                    let stringDataList = JSON.stringify(parsedJsonResponse.data.list);
-                    let parsedBuffer = JSON.parse(stringDataList);
-                    let parsedJsonDataList = JSON.parse(parsedBuffer);
+                let stringResponse = JSON.stringify(response.data); // get response as string
+                let parsedJsonResponse = JSON.parse(stringResponse);    // parse response
+                if (parsedJsonResponse.status === true) {   // check ok response
+                    let stringDataList = JSON.stringify(parsedJsonResponse.data.list);  // get string data
+                    // for some reason we have to use parsing 2 times
+                    let parsedBuffer = JSON.parse(stringDataList);  // parse into buffer
+                    let parsedJsonDataList = JSON.parse(parsedBuffer);  // parse buffer as json
+                    // set currency
                     setSelectedCurrency(parsedJsonResponse.data.currency);
+                    // set price
                     setIsEmptyPrice(parsedJsonResponse.data.isEmptyPrice);
+                    // set full data list for later use
                     setDataList(parsedJsonDataList);
+                    // init arrays
                     let date: any[] = [];
                     let income: any[] = [];
-                    var expenses: any[] = [];
+                    let expenses: any[] = [];
+                    // parse data list into sublist
                     for (let index = 0; index < parsedJsonDataList.length; index ++) {
+                        // update arrays with data
                         date.push(parsedJsonDataList[index].date);
                         income.push(parsedJsonDataList[index].income);
                         expenses.push(parsedJsonDataList[index].expenses);
                     }
+                    // set list of dates
                     setDateList(date);
+                    // set list of incomes
                     setIncomeList(income);
+                    // set list of expenses
                     setExpensesList(expenses);
+                    //setGraphData();
                 }
             }
         }
@@ -110,7 +137,7 @@ export default function Dashboard() {
     return (
         <div className='dashboard'>
             <h1>Welcome to the main Dashboard</h1>
-
+            <Line redraw={true} options={options} data={graphData}/>
         </div>
   );
 }
